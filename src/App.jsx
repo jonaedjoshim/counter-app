b356dbdad59529dc4d0756b3caec2c0e9e03afec
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-30 23:52:35 +0600

    added conditional rendaring

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ function App() {
     <div className='w-11/12 mx-auto flex flex-col items-center text-center justify-center min-h-screen gap-8 shadow-lg p-10'>
       <h2 className='font-bold text-2xl'>Counter App</h2>
       <Runs run={runs} singles={singles} fours={fours} sixes={sixes} />
+
+      {
+        runs >= 100 ? <p className='font-bold text-2xl text-green-500'>Congratulations! You have reached 100 runs!</p> : runs >= 50 ? <p className='font-bold text-2xl text-yellow-500'>Great! You have reached 50 runs!</p> : ''
+      }
+
       <Buttons
         runs={runs}
         setRuns={setRuns}
